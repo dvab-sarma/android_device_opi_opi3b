0152dc775a488cc5ad452f9d492358e517b734b7
dvab-sarma <vdarbha0473@gmail.com>
2025-08-06 15:22:20 -0500

    modified the mkimg.sh file to support Orange pi 3b and it can be linked to a file in the root directory

--- mkimg.sh
+++ mkimg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2021-2022 KonstaKANG
+# Copyright (C) 2025 Venkata Atchuta Bheemeswara Sarma Darbha
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,6 +11,9 @@ exit_with_error() {
   exit 1
 }
 
+
+
+# Check required env vars
 if [ -z ${TARGET_PRODUCT} ]; then
   exit_with_error "TARGET_PRODUCT environment variable is not set. Run lunch first."
 fi
@@ -19,18 +22,26 @@ if [ -z ${ANDROID_PRODUCT_OUT} ]; then
   exit_with_error "ANDROID_PRODUCT_OUT environment variable is not set. Run lunch first."
 fi
 
+# Check required images
 for PARTITION in "boot" "system" "vendor"; do
   if [ ! -f ${ANDROID_PRODUCT_OUT}/${PARTITION}.img ]; then
-    exit_with_error "Partition image not found. Run 'make ${PARTITION}image' first."
+    exit_with_error "Partition image not found: ${PARTITION}.img. Run 'make ${PARTITION}image' first."
   fi
 done
 
-VERSION=RaspberryVanillaAOSP15
+UBOOT_BIN=device/opi/opi3b-kernel/u-boot-rockchip.bin
+if [ ! -f ${UBOOT_BIN} ]; then
+  exit_with_error "Missing u-boot-rockchip.bin! Make sure it's built and placed at ${UBOOT_BIN}"
+fi
+
+# Image metadata
+VERSION=OrangePiAOSP
 DATE=$(date +%Y%m%d)
 TARGET=$(echo ${TARGET_PRODUCT} | sed 's/^aosp_//')
 IMGNAME=${VERSION}-${DATE}-${TARGET}.img
 IMGSIZE=14848MiB
 
+# Avoid overwriting existing image
 if [ -f ${ANDROID_PRODUCT_OUT}/${IMGNAME} ]; then
   exit_with_error "${ANDROID_PRODUCT_OUT}/${IMGNAME} already exists!"
 fi
@@ -39,6 +50,12 @@ echo "Creating image file ${ANDROID_PRODUCT_OUT}/${IMGNAME}..."
 sudo fallocate -l ${IMGSIZE} ${ANDROID_PRODUCT_OUT}/${IMGNAME}
 sync
 
+# Write U-Boot at sector 64 (32KB offset)
+echo "Writing U-Boot to sector 64..."
+sudo dd if=${UBOOT_BIN} of=${ANDROID_PRODUCT_OUT}/${IMGNAME} seek=64 bs=512 conv=notrunc
+sync
+
+# Create partitions
 echo "Creating partitions..."
 (
 echo o
@@ -70,6 +87,7 @@ echo w
 ) | sudo fdisk ${ANDROID_PRODUCT_OUT}/${IMGNAME}
 sync
 
+# Mount loop device
 LOOPDEV=$(sudo kpartx -av ${ANDROID_PRODUCT_OUT}/${IMGNAME} | awk 'NR==1{ sub(/p[0-9]$/, "", $3); print $3 }')
 if [ -z ${LOOPDEV} ]; then
   exit_with_error "Unable to find loop device!"
@@ -77,18 +95,23 @@ fi
 echo "Image mounted as /dev/${LOOPDEV}"
 sleep 1
 
+# Write images to partitions
 echo "Copying boot..."
 sudo dd if=${ANDROID_PRODUCT_OUT}/boot.img of=/dev/mapper/${LOOPDEV}p1 bs=1M
+
 echo "Copying system..."
 sudo dd if=${ANDROID_PRODUCT_OUT}/system.img of=/dev/mapper/${LOOPDEV}p2 bs=1M
 echo "Copying vendor..."
 sudo dd if=${ANDROID_PRODUCT_OUT}/vendor.img of=/dev/mapper/${LOOPDEV}p3 bs=1M
+
+# Create userdata partition
 echo "Creating userdata..."
 sudo mkfs.ext4 /dev/mapper/${LOOPDEV}p4 -I 512 -L userdata
 sync
 
+# Unmount loop device
 sudo kpartx -d "/dev/${LOOPDEV}"
 sudo chown ${USER}:${USER} ${ANDROID_PRODUCT_OUT}/${IMGNAME}
 
-echo "Done, created ${ANDROID_PRODUCT_OUT}/${IMGNAME}!"
+echo "✅ Done! Created ${ANDROID_PRODUCT_OUT}/${IMGNAME} ready to flash with u-boot pre-installed."
 exit 0
